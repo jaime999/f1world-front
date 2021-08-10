d98854b99f57656673f2c79ca80f4c2d6a1e3f42
jaime999 <45595827+jaime999@users.noreply.github.com>
2021-08-10 21:48:33 +0200

    Added teams clasification | style table in clasificationTable

--- src/components/MainPage/clasification.vue
+++ src/components/MainPage/clasification.vue
@@ -14,13 +14,11 @@
 
       <q-tab-panels v-model="tab" animated>
         <q-tab-panel name="drivers">
-          <clasification-table></clasification-table>
+          <clasification-table :data="drivers"></clasification-table>
         </q-tab-panel>
 
         <q-tab-panel name="constructors">
-          With so much content to display at once, and often so little screen real-estate,
-          Cards have fast become the design pattern of choice for many companies, including
-          the likes of Google and Twitter.
+          <clasification-table :data="teams"></clasification-table>
         </q-tab-panel>
       </q-tab-panels>
     </q-card>
@@ -30,6 +28,62 @@
 <script>
 import { ref } from 'vue';
 import clasificationTable from './clasificationTable.vue';
+
+const drivers = [
+  {
+    index: 1,
+    name: 'L. Hamilton',
+    points: 195,
+  },
+  {
+    index: 2,
+    name: 'M. Verstappen',
+    points: 187,
+  },
+  {
+    index: 3,
+    name: 'L. Norris',
+    points: 113,
+  },
+  {
+    index: 4,
+    name: 'V. Bottas',
+    points: 108,
+  },
+  {
+    index: 5,
+    name: 'S. Pérez',
+    points: 104,
+  },
+];
+
+const teams = [
+  {
+    index: 1,
+    name: 'Mercedes',
+    points: 303,
+  },
+  {
+    index: 2,
+    name: 'Red Bull',
+    points: 291,
+  },
+  {
+    index: 3,
+    name: 'Ferrari',
+    points: 163,
+  },
+  {
+    index: 4,
+    name: 'McLaren',
+    points: 163,
+  },
+  {
+    index: 5,
+    name: 'Alpine',
+    points: 77,
+  },
+];
 
 const data = () => ({
   tab: ref('drivers'),
@@ -41,5 +95,17 @@ export default {
   components: {
     clasificationTable,
   },
+  setup() {
+    return {
+      drivers,
+      teams,
+    };
+  },
 };
 </script>
+
+<style>
+.q-tab-panel {
+  padding: 4px 16px;
+}
+</style>
--- src/components/MainPage/clasificationTable.vue
+++ src/components/MainPage/clasificationTable.vue
@@ -1,9 +1,9 @@
 <template>
     <q-table
-      :rows="rows"
+      :rows="data"
       :columns="columns"
       row-key="index"
-      virtual-scroll
+      flat
       :rows-per-page-options="[0]"
       hide-header
       hide-bottom
@@ -11,6 +11,13 @@
 </template>
 
 <script>
+
+const props = {
+  data: {
+    type: Array,
+    default: () => [],
+  },
+};
 const columns = [
   {
     name: 'index',
@@ -31,41 +38,13 @@ const columns = [
   },
 ];
 
-const rows = [
-  {
-    index: 1,
-    name: 'L. Hamilton',
-    points: 195,
-  },
-  {
-    index: 2,
-    name: 'M. Verstappen',
-    points: 187,
-  },
-  {
-    index: 3,
-    name: 'L. Norris',
-    points: 113,
-  },
-  {
-    index: 4,
-    name: 'V. Bottas',
-    points: 108,
-  },
-  {
-    index: 5,
-    name: 'S. Pérez',
-    points: 104,
-  },
-];
-
 export default {
   name: 'ClasificationTable',
   setup() {
     return {
       columns,
-      rows,
     };
   },
+  props,
 };
 </script>
